5bfd064064866b2466cc4c3501942280ba708061
lyka-guillen-a <62961193+lyka-guillen-a@users.noreply.github.com>
2021-02-17 22:34:15 -0600

    x

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { FormDialogComponent } from '../dialogs/form-dialog/form-dialog.component';
+import { DestinosDialogComponent } from '../dialogs/destinos-dialog/destinos-dialog.component';
+//import { DestinosDialogComponent } from '../dialogs/destinos-dialog/destinos-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
   }
+
+//openDialogLogin
+
+openDialogLogin(): void {
+  const dialogRef = this.dialog.open(FormDialogComponent);
+}
+
+//openDialogAddDestino
+openDialogAddDestino(): void {
+  const dialogRef = this.dialog.open(DestinosDialogComponent);
+}
+
+//openDialogResult
+openDialogResult(): void {
+  const dialogRef = this.dialog.open(DestinosDialogComponent);
+}
+
 
 }
